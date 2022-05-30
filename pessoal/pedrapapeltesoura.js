b305405703aeaf129e2c9574c40b82b7758a20e2
pedrOfsilveira <105888557+pedrOfsilveira@users.noreply.github.com>
2022-05-30 19:50:39 -0300

    Formatting

--- pessoal/pedrapapeltesoura.js
+++ pessoal/pedrapapeltesoura.js
@@ -1,24 +1,35 @@
-var ans, ppt;
+var ans, ppt, again;
 
-ppt = parseInt(Math.random() * 3);
-ans = parseInt(prompt('Escolha:\n1.PEDRA\n2.PAPEL\n3.TESOURA'));
+again = 1;
 
-if (ans == 0 && ppt == 0) {
-  alert('VOCÊ JOGOU: PEDRA\nO COMPUTADOR JOGOU: PEDRA\n\nEMPATE!');
-} else if (ans == 0 && ppt == 1) {
-  alert('VOCÊ JOGOU: PEDRA\nO COMPUTADOR JOGOU: PAPEL\n\nVOCÊ PERDEU!');
-} else if (ans == 0 && ppt == 2) {
-  alert('VOCÊ JOGOU: PEDRA\nO COMPUTADOR JOGOU: TESOURA\nVOCÊ GANHOU!');
-} else if (ans == 1 && ppt == 0) {
-  alert('VOCÊ JOGOU: PAPEL\nO COMPUTADOR JOGOU: PEDRA\n\nVOCÊ GANHOU!');
-} else if (ans == 1 && ppt == 1) {
-  alert('VOCÊ JOGOU: PAPEL\nO COMPUTADOR JOGOU: PAPEL\n\nEMPATE!');
-} else if (ans == 1 && ppt == 2) {
-  alert('VOCÊ JOGOU: PAPEL\nO COMPUTADOR JOGOU: TESOURA\n\nVOCÊ PERDEU!');
-} else if (ans == 2 && ppt == 0) {
-  alert('VOCÊ JOGOU: TESOURA\nO COMPUTADOR JOGOU: PEDRA\n\nVOCÊ PERDEU!');
-} else if (ans == 2 && ppt == 1) {
-  alert('VOCÊ JOGOU: TESOURA\nO COMPUTADOR JOGOU: PAPEL\n\nVOCÊ GANHOU!');
-} else if (ans == 2 && ppt == 2) {
-  alert('VOCÊ JOGOU: TESOURA\nO COMPUTADOR JOGOU: TESOURA\n\nEMPATE!');
+while (again == 1) {
+  ppt = parseInt(Math.random() * 3);
+
+  do {
+    ans = parseInt(prompt('Escolha:\n0.PEDRA\n1.PAPEL\n2.TESOURA'));
+  } while (ans != 0 && ans != 1 && ans != 3);
+
+  if (ans == 0 && ppt == 0) {
+    alert('VOCÊ JOGOU: PEDRA\nO COMPUTADOR JOGOU: PEDRA\n\nEMPATE!');
+  } else if (ans == 0 && ppt == 1) {
+    alert('VOCÊ JOGOU: PEDRA\nO COMPUTADOR JOGOU: PAPEL\n\nVOCÊ PERDEU!');
+  } else if (ans == 0 && ppt == 2) {
+    alert('VOCÊ JOGOU: PEDRA\nO COMPUTADOR JOGOU: TESOURA\nVOCÊ GANHOU!');
+  } else if (ans == 1 && ppt == 0) {
+    alert('VOCÊ JOGOU: PAPEL\nO COMPUTADOR JOGOU: PEDRA\n\nVOCÊ GANHOU!');
+  } else if (ans == 1 && ppt == 1) {
+    alert('VOCÊ JOGOU: PAPEL\nO COMPUTADOR JOGOU: PAPEL\n\nEMPATE!');
+  } else if (ans == 1 && ppt == 2) {
+    alert('VOCÊ JOGOU: PAPEL\nO COMPUTADOR JOGOU: TESOURA\n\nVOCÊ PERDEU!');
+  } else if (ans == 2 && ppt == 0) {
+    alert('VOCÊ JOGOU: TESOURA\nO COMPUTADOR JOGOU: PEDRA\n\nVOCÊ PERDEU!');
+  } else if (ans == 2 && ppt == 1) {
+    alert('VOCÊ JOGOU: TESOURA\nO COMPUTADOR JOGOU: PAPEL\n\nVOCÊ GANHOU!');
+  } else if (ans == 2 && ppt == 2) {
+    alert('VOCÊ JOGOU: TESOURA\nO COMPUTADOR JOGOU: TESOURA\n\nEMPATE!');
+  }
+
+  do {
+    again = parseInt(prompt('Deseja jogar novamente?\n1. SIM\n2. NÃO'));
+  } while (again != 1 && again != 2);
 }
